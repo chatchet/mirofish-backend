268b6fa61cabb86e85c75df1356edf7bd5e2d98a
chatchet <yu.digital.tools@gmail.com>
2026-04-23 08:30:09 +0800

    Update status.py

--- services/status.py
+++ services/status.py
@@ -1,15 +1,8 @@
-# 🕒 Singapore Time: 2026-04-22 11:34
-# =========================================
-# Status endpoint
-# =========================================
-
 from flask import Blueprint
 import os
 
-# 定义 Blueprint（必须有）
 status_bp = Blueprint('status', __name__, url_prefix='/api')
 
-# 路由
 @status_bp.route('/status')
 def status():
     return {
